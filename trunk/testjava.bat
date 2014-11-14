8aa92f882af96cdeb6201b934ea036a42959b9f6
royandregundersen@yahoo.no <royandregundersen@yahoo.no@605fab5a-42c6-23a8-2efa-01c7e4e784bc>
2014-11-14 21:22:09 +0000

    java: use resource/classpath for assets

--- trunk/testjava.bat
+++ trunk/testjava.bat
@@ -1,2 +1,3 @@
-haxe -java outjava -cp . -main Main
-"\Program Files (x86)\Java\jre7\bin\java.exe" -jar outjava\Main.jar
+haxe -java outjava -cp . -main Main -debug
+"%JAVA_HOME%\bin\jar.exe" uvf outjava\Main-Debug.jar assets
+"\Program Files (x86)\Java\jre7\bin\java.exe" -jar outjava\Main-Debug.jar
