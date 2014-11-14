f9581029554d384a94b4915454e448e531539f21
royandregundersen <royandregundersen@yahoo.no>
2014-11-14 21:54:11 +0000

    java/php Assets: First try Resource then file

--- com/leapinglight/io/Assets.hx
+++ com/leapinglight/io/Assets.hx
@@ -4,7 +4,7 @@ package com.leapinglight.io;
 typedef Assets = openfl.Assets;
 #else
 
-#if java
+#if (php||java)
 import haxe.Resource;
 #end
 
@@ -14,16 +14,32 @@ class Assets
 {
 
 	public static function getBytes(path:String) {
-		#if java
-		return new ByteArray(Resource.getBytes(path));
+		#if (php || java)
+		var path2 = path;
+		#if php
+		path2 = StringTools.replace(path2, "/", "_") + ".pl";
+		#end
+		try {
+			return new ByteArray(Resource.getBytes(path2));
+		} catch (e:Dynamic) {
+			return new ByteArray(File.getBytes(path));
+		}
 		#else
 		return new ByteArray(File.getBytes(path));
 		#end
 	}
 	
 	public static function getText(path:String) {
-		#if java
-		return Resource.getString(path);
+		#if (php || java)
+		var path2 = path;
+		#if php
+		path2 = StringTools.replace(path2, "/", "_") + ".pl";
+		#end
+		try {
+			return Resource.getString(path2);
+		} catch (e:Dynamic) {
+			return File.getContent(path);
+		}
 		#else
 		return File.getContent(path);
 		#end
