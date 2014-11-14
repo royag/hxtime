5044c1fc08fb748db471e2fc822b5de28d2e7ec1
royandregundersen <royandregundersen@yahoo.no>
2014-11-14 21:22:09 +0000

    java: use resource/classpath for assets

--- com/leapinglight/io/Assets.hx
+++ com/leapinglight/io/Assets.hx
@@ -4,17 +4,29 @@ package com.leapinglight.io;
 typedef Assets = openfl.Assets;
 #else
 
+#if java
+import haxe.Resource;
+#end
+
 import sys.io.File;
 
 class Assets
 {
 
 	public static function getBytes(path:String) {
+		#if java
+		return new ByteArray(Resource.getBytes(path));
+		#else
 		return new ByteArray(File.getBytes(path));
+		#end
 	}
 	
 	public static function getText(path:String) {
+		#if java
+		return Resource.getString(path);
+		#else
 		return File.getContent(path);
+		#end
 	}
 	
 }
